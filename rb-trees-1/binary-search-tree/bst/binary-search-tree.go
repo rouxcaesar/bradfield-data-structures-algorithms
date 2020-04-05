553a54e83f754ab5c773708e6ee3db398f228bac
rouxcaesar <jrzerwick@gmail.com>
2020-04-05 11:47:30 -0400

    Added files for binary search tree implementation

---
+++ rb-trees-1/binary-search-tree/bst/binary-search-tree.go
@@ -0,0 +1,12 @@
+package bst
+
+import "fmt"
+
+type Node struct {
+	value       int
+	left, right *Node
+}
+
+func (n *Node) Find(i int) (int, bool) {
+
+}
---
+++ rb-trees-1/binary-search-tree/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//slice := qs.GenerateSlice(10, "random")
+	slice := []int{2, 7, 9, 1, 4, 6}
+	fmt.Printf("---------Slice-----------\n")
+	fmt.Println(slice)
+	fmt.Printf("---------Binary Search Tree-----------\n")
+	fmt.Println("Not yet implemented")
+}
